c3a914554799d11282de2dcdeb5985d049a4c981
gabor-szeles <szelesgbr@gmail.com>
2017-10-03 12:18:13 +0200

    resident modals are fully functional

--- static/planets.js
+++ static/planets.js
@@ -27,7 +27,37 @@ function pager(page){
 
 $('#residentsModal').on('show.bs.modal', function(event) {
     var clickedButton = $(event.relatedTarget) // Button that triggered the modal
-    console.log(clickedButton)
     var planetName = clickedButton.data('planetname')
+    var apiList = clickedButton.data('residentapilist')
+    apiList = apiList.replace(/'/g, '').slice(1, -1).split(',')
+    var residentsArray = []
+    for (let api=0; api<apiList.length;api++){
+        apiCall(apiList[api])
+    }
     $('.modal-title').text(`Known residents of ${planetName}`)
 });
+
+
+$("#residentsModal").on("hidden.bs.modal", function () {
+    $(".actual-data").empty()
+});
+
+
+function apiCall(apiLink){
+    $.ajax({
+        dataType: "json",
+        url: apiLink,
+        success: function(response) {
+            $(".resident-data").append(`<tr class="actual-data">
+                                            <td>${response['name']}</td>
+                                            <td>${response['height']}</td>
+                                            <td>${response['mass']}</td>
+                                            <td>${response['skin_color']}</td>
+                                            <td>${response['hair_color']}</td>
+                                            <td>${response['eye_color']}</td>
+                                            <td>${response['birth_year']}</td>
+                                            <td>${response['gender']}</td>
+                                        </tr>`)
+        }
+    });
+}
