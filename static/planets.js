cb495f025c8b7ac2509605c2d0c607a3cb2237e4
gabor-szeles <szelesgbr@gmail.com>
2017-10-03 10:55:09 +0200

    empty modals implemented

--- static/planets.js
+++ static/planets.js
@@ -1,13 +1,33 @@
 $('#nextButton').click(function(){
-    var nextPage = document.getElementById('nextButton').dataset.next
-    console.log(nextPage)
+    var nextPage = this.dataset.next
+    pager(nextPage)
+
+})
+
+
+$('#prevButton').click(function(){
+    var prevPage = this.dataset.prev
+    pager(prevPage)
+
+})
+
+
+function pager(page){
     $.ajax({
       type: 'POST',
       url: '/',
-      data: {"next_page": nextPage}
+      data: {"next_page": page}
     })
      .done(function(completeHtmlPage) {
        $("body").empty();
        $("body").append(completeHtmlPage)
     });
-})
+}
+
+
+$('#residentsModal').on('show.bs.modal', function(event) {
+    var clickedButton = $(event.relatedTarget) // Button that triggered the modal
+    console.log(clickedButton)
+    var planetName = clickedButton.data('planetname')
+    $('.modal-title').text(`Known residents of ${planetName}`)
+});
