148b7a3d25478b9c505c3aaef6d36ba92a31c8e3
gabor-szeles <szelesgbr@gmail.com>
2017-10-04 16:26:02 +0200

    reverted connection to database to legacy way

--- database.py
+++ database.py
@@ -2,22 +2,13 @@ from config import Config
 from datetime import datetime
 import psycopg2
 import psycopg2.extras
-import os
-import psycopg2
-import urllib
 
 
 def open_database():
     try:
-        urllib.parse.uses_netloc.append('postgres')
-        url = urllib.parse.urlparse(os.environ.get('DATABASE_URL'))
-        connection = psycopg2.connect(
-            database=url.path[1:],
-            user=url.username,
-            password=url.password,
-            host=url.hostname,
-            port=url.port
-            )
+        connection_string = Config.DB_CONNECTION_STR
+        connection = psycopg2.connect(connection_string)
+        connection.autocommit = True
     except psycopg2.DatabaseError as exception:
         print(exception)
     return connection
